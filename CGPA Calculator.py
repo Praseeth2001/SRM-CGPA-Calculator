3f59e9e8001d266dd62f4c6f7707b9ed326029a8
Vaibhav Agarwal <58652650+vaibhavagarwal47@users.noreply.github.com>
2021-10-04 13:31:17 +0530

    Update CGPA Calculator.py

--- CGPA Calculator.py
+++ CGPA Calculator.py
@@ -6,7 +6,7 @@ global root
 
 def calculate():
     total_grades = 0
-    total_credits = int(n1.get()) + int(n2.get()) + int(n3.get()) + int(n4.get()) + int(n5.get())
+    total_credits = int(n1.get()) + int(n2.get()) + int(n3.get()) + int(n4.get()) + int(n5.get()) + int(n6.get()) + int(n7.get())
     if g1.get() == 'O':
         total_grades += 10*int(n1.get())
     if g1.get() == 'A+':
@@ -47,6 +47,22 @@ def calculate():
         total_grades += 8*int(n5.get())
     if g5.get() == 'B+':
         total_grades += 7*int(n5.get())
+    if g6.get() == 'O':
+        total_grades += 10*int(n6.get())
+    if g6.get() == 'A+':
+        total_grades += 9*int(n6.get())
+    if g6.get() == 'A':
+        total_grades += 8*int(n6.get())
+    if g6.get() == 'B+':
+        total_grades += 7*int(n6.get())
+    if g7.get() == 'O':
+        total_grades += 10*int(n7.get())
+    if g7.get() == 'A+':
+        total_grades += 9*int(n7.get())
+    if g7.get() == 'A':
+        total_grades += 8*int(n7.get())
+    if g7.get() == 'B+':
+        total_grades += 7*int(n7.get())
     
     
     try:
@@ -63,7 +79,7 @@ def calculate():
 if __name__ == '__main__':
     root = Tk()
     root.geometry('500x700')
-    root.title('Lab 4')
+    root.title('SRM SGPA Calculator')
     root.resizable(False, False)
 
     srm_GPA_CALCULATOR = Label(root, text = "SRM GPA CALCULATOR", font = ('Sans Serif', 18, 'bold'))
@@ -78,10 +94,10 @@ if __name__ == '__main__':
     grade_obt = Label(root, text = 'Grades Obtained', font = ('Sans Serif', 14))
     grade_obt.place(x=350, y=100)
 
-    global n1, n2, n3, n4, n5
-    n1, n2, n3, n4, n5 = StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), 
-    global g1, g2, g3, g4, g5
-    g1, g2, g3, g4, g5 = StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), 
+    global n1, n2, n3, n4, n5, n6, n7
+    n1, n2, n3, n4, n5, n6, n7 = StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar()
+    global g1, g2, g3, g4, g5, g6, g7
+    g1, g2, g3, g4, g5, g6, g7 = StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar() 
 #Number 1
     numbering1 = Label(root, text='1.')
     numbering1.place(x=40, y=150)
@@ -136,6 +152,26 @@ if __name__ == '__main__':
     grade_score5 = ttk.Combobox(root, width=5, textvariable = g5)
     grade_score5.place(x=380, y=350)
     grade_score5['values'] = ('O', 'A+', 'A', 'B+')
+#Number 6
+    numbering6 = Label(root, text='6.')
+    numbering6.place(x=40, y=400)
+
+    credits_score6 = Spinbox(root, from_=0, to = 5, width=3, textvariable = n6)
+    credits_score6.place(x=220, y=400)
+
+    grade_score6 = ttk.Combobox(root, width=5, textvariable = g6)
+    grade_score6.place(x=380, y=400)
+    grade_score6['values'] = ('O', 'A+', 'A', 'B+')
+#Number 7
+    numbering7 = Label(root, text='7.')
+    numbering7.place(x=40, y=450)
+
+    credits_score7 = Spinbox(root, from_=0, to = 5, width=3, textvariable = n7)
+    credits_score7.place(x=220, y=450)
+
+    grade_score7 = ttk.Combobox(root, width=5, textvariable = g7)
+    grade_score7.place(x=380, y=450)
+    grade_score7['values'] = ('O', 'A+', 'A', 'B+')
 
     cal = Button(root, text='CALCULATE', command = calculate)
     cal.place(x=200, y=600)
